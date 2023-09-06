24a4a0e06ad43af57d6ec1875457fc6a9c6b9241
Aiman Ali <70829107+Aiman220@users.noreply.github.com>
2023-09-06 17:36:39 +0300

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -13,7 +13,7 @@ mongoose.connect(process.env.MONGO_URL, () => console.log('MongoDB has been star
 
 app.use(cors(
     {
-        origin: ['https://genreglider.onrender.com/']
+        origin: ['https://genregliderserver.vercel.app/']
     }
     ));
 
